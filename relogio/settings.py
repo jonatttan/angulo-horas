3013cc598680465243042cdbf96e7046ee82d013
jonatttan <jonattan.sousa@gmail.com>
2021-01-13 02:15:19 -0300

    Alterado projeto para rodar no Heroku, primeiro commit2

--- relogio/settings.py
+++ relogio/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-import json
+
 import os
 import django_heroku
 from pathlib import Path
@@ -18,21 +18,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
-    secrets = json.load(secrets_file)
-
-def get_secret(setting, secrets=secrets):
-    """Get secret setting or fail with ImproperlyConfigured"""
-    try:
-        return secrets[setting]
-    except KeyError:
-        raise ImproperlyConfigured("Set the {} setting".format(setting))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = get_secret('SECRET_KEY')
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 
